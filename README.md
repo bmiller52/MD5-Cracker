f0cbac09cb4dc06dede54bfe8b9381a70d1a393e
bmiller52 <millerbam52@gmail.com>
2017-04-22 09:49:50 -0400

    Added time to mains, update readme

--- README.md
+++ README.md
@@ -8,4 +8,4 @@
 #
 # Then enter the .txt file name for the script to run through to find the password. I’ve included a file that has the top 1 million most common passwords. I ran through that file in about 5 seconds.
 #
-# Here’s a website that has basically every combination. The small file has over 63 million passwords. I ran through the entire thing in about 6 and a half minutes. Some computers may be slower. There's also a bigger one which I don't know how many passwords it has. https://crackstation.net/buy-crackstation-wordlist-password-cracking-dictionary.htm
+# Here’s a website that has basically every combination. The small file has over 63 million passwords. I ran through the entire thing in about 6 and a half minutes. Some computers may be slower. There's also a bigger one. I estimated the file size times how many passwords was in the small one. It’s about 18 billion and take about 5 hours. https://crackstation.net/buy-crackstation-wordlist-password-cracking-dictionary.htm
--- md5_cracker.py
+++ md5_cracker.py
@@ -1,4 +1,12 @@
 import md5
+from datetime import datetime
+import time
+from time import sleep
+
+now = datetime.now()
+the_hour = now.hour
+the_min = now.minute
+the_sec = now.second
 
 counter = 1
 
@@ -19,7 +27,23 @@ for password in pwfile:
 
     if pass_in == filemd5:
         print("\nMatch Found. \nPassword is: %s") % password
+
+        now2 = datetime.now()
+        the_hour2 = now2.hour
+        the_min2 = now2.minute
+        the_sec2 = now2.second
+
+        print('\nStarted at ' + str(the_hour) + ":" + str(the_min) + "." + str(the_sec))
+        print('Ended at ' + str(the_hour2) + ":" + str(the_min2) + "." + str(the_sec2) + "\n")
         break
 
 else:
+    now2 = datetime.now()
+    the_hour2 = now2.hour
+    the_min2 = now2.minute
+    the_sec2 = now2.second
+
     print("\nPassword Not Found")
+
+    print('\nStarted at ' + str(the_hour) + ":" + str(the_min) + "." + str(the_sec))
+    print('Ended at ' + str(the_hour2) + ":" + str(the_min2) + "." + str(the_sec2) + "\n")
